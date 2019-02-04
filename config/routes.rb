19e54c635aeb8d623a454fc9f06ef9dc7bc714b1
Emma Calvet <emma.calvet.g@gmail.com>
2019-02-04 17:19:21 +0100

    team et contact OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#display'
+  get 'welcome/:id', to: 'welcome#greetings'
+  get '/contact', to: 'contact#display'
+  get '/team', to: 'team#display'
 end
