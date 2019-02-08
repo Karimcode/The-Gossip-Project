a19084c5dfb150b739a09e0ff1ca4db3eb84284b
Jimmy Charpagne <jimmy.charpagne@gmail.com>
2019-02-08 02:56:41 +0100

    leger clean

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,4 @@
 class UsersController < ApplicationController
-#  def display
-#    @user_nb = User.find_by(id: params[:user_id])
-#    @city = City.find(@user_nb.city_id).name
-#    @user = nil
-#    User.all.each do |author|
-#      @user = author
-#    end
-#  end
 
 def index
   # Méthode qui récupère tous les potins et les envoie à la view index (index.html.erb) pour affichage
